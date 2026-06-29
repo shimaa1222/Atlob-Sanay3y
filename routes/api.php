4462c4303ed57f3420421657ebbbaa9eae1888d3
shimaa <72949892+shimaa1222@users.noreply.github.com>
2026-06-29 23:14:11 +0300

    Reorder use statements for API controllers

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Api\Auth\AuthController;
-use App\Http\Controllers\Api\Booking\BookingController; // ✅ Api
-use App\Http\Controllers\Api\Craftsman\CraftsmanController; // ✅ Api
-use App\Http\Controllers\Api\ServicePost\ServicePostController; // ✅ Api
+use App\Http\Controllers\Api\Booking\BookingController;
+use App\Http\Controllers\Api\Craftsman\CraftsmanController;
+use App\Http\Controllers\Api\ServicePost\ServicePostController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Admin\AdminSettingsController;
 use Illuminate\Support\Facades\Route;
